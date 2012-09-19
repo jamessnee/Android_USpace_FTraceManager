957f67279c6df8ef4332cd1061cbc0c39cd2650f
James Snee <jas250@cl.cam.ac.uk>
2012-09-19 11:59:56 +0100

    Fixed bug that could cause a possible race condition on the file descriptors. Tidied things up a little

--- Native/ftrace_pipe_reader/ftrace_fpipe_reader.c
+++ Native/ftrace_pipe_reader/ftrace_fpipe_reader.c
@@ -2,20 +2,25 @@
 
 
 main(int argc, char *argv[]){
-	FILE *trace_pipe_fp;
-	FILE *output_fp;
 	
-	output_fp = fopen("/data/trace_output.txt","w+");
-	trace_pipe_fp = fopen("/sys/kernel/debug/tracing/trace_pipe","r");
-	if(trace_pipe_fp != NULL && output_fp!=NULL){
-		char line[128];
+	//Setup the output file
+	FILE *output_fp = fopen(argv[1],"w+");
+	if(output_fp == NULL){
+		printf("File doesn't exist, attempting to create it...");	
+	}
 
+	//Setup the file pointer to the trace pipe
+	FILE *trace_pipe_fp = fopen("/sys/kernel/debug/tracing/trace_pipe","r");
+	if(trace_pipe_fp!=NULL){
+
+		//Start reading from the pipe
+		char line[128];
 		while(fgets(line,sizeof line,trace_pipe_fp)!=NULL){
-			//fputs(line,stdout);
-			fprintf(output_fp,"%s",line);
+			fputs(line,stdout);
+			//fprintf(output_fp,"%s",line);
 		}
 		fclose(trace_pipe_fp);
 	}else{
-		printf("Couldn't open file for reading or writing! \n");
+		printf("Couldn't open the trace pipe for reading...");
 	}
 }
